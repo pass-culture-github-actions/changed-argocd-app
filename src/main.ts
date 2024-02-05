b111e3e5861eb6c7a1ad577137175274780b1fa3
Dimitri Kopriwa <kopx@free.fr>
2024-02-05 11:01:56 +0100

    fix: detection

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { GetResponseDataTypeFromEndpointMethod } from '@octokit/types';
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
 
-
 async function run() {
   try {
     const allModifiedFiles = core.getInput('all_modified_files').split(' ');
@@ -17,6 +16,7 @@ async function run() {
 
       const { stdout, stderr } = await execa('argocd', ['app', 'list', '--core', '--output', 'json']);
       if (stderr) {
+        core.debug(stderr)
         throw new Error(stderr)
       }
       const json = JSON.parse(stdout) // key = path, value = name
@@ -26,20 +26,24 @@ async function run() {
         map.set(appPath, map.has(appPath) ? [...map.get(appPath), appName] : [appName])
       }
 
+
       for (const allModifiedFile of allModifiedFiles) {
         for (const [key, value] of map) {
+          core.debug(`file=${allModifiedFile}, mapKey=${key}, mapValue=${value.join(',')}`)
           if (`./${allModifiedFile}`.includes(key)) {
             apps.push(value.find((v: string) => allModifiedFile.includes(v)))
           }
         }
       }
 
+      core.debug(`apps=${apps.join(',')}`)
       core.setOutput('apps', apps.join(' '));
     }
 
     await createApps();
   } catch (error) {
     if (error instanceof Error) {
+      core.debug(error.message)
       core.setFailed(error.message);
     }
   }
